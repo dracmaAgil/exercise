5221149703eae262c7b97c2c343c5536423297fc
Julian Montes <macrodracma@gmail.com>
2015-04-23 08:46:04 -0500

    some stuff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    @posts = Post.all.search(params[:q]).result
+    # @search = Post.all.search(params[:q]).result
+    # @search = Post.ransack(params[:q])
+    # @search.sorts = 'created_at desc' if @search.sorts.empty?
+    # @post = @search.result.paginate(page: params[:page], per_page: 20)
+    @posts = Post.all.order('created_at desc')
   end
 end
